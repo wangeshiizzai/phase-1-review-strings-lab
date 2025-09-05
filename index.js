77cd81e2500841e3f53d03873eee01057914ea75
wangeshiizzai <keshi.yvonne84@gmail.com>
2025-09-05 17:25:17 +0300

    Completed 2nd Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code in this file!
+const currentUser = `Wangeci`;
+
+const welcomeMessage = `Welcome to Flatbook, `;
+
+const welcomeMessage = currentUser;
+
+const welcomeMessage = `welcome to Flatbook, currentUser`;
+
+const welcomeMessage = `Welcome to Flatbook, ` + currentUser;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}`;
+
